2751b703dfb3c76cbfe4bbe5874a3f09886f1458
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-04-09 19:00:00 +0100

    chore: Update project settings
    - Adjust pagination size to 2 items per page\n- Clean up REST framework settings\n- Update installed apps configuration

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+   
     'djoser',
     'LittleLemonAPI',
 ]
@@ -143,7 +144,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
+    "PAGE_SIZE": 2,
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.UserRateThrottle",
         "rest_framework.throttling.AnonRateThrottle",
