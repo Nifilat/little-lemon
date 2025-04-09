1700ef2c06e9bd89c8c62b15ea7a03484218bfac
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-04-09 18:59:29 +0100

    feat: Update models and serializers
    - Add string representation to MenuItem model\n- Simplify MenuItemSerializer with PrimaryKeyRelatedField\n- Update CartSerializer with price validation\n- Add UserSerializer for group management\n- Refactor OrderSerializer for better data handling

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -15,6 +15,9 @@ class MenuItem(models.Model):
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     
+    def __str__(self):
+        return self.title
+    
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
@@ -32,6 +35,8 @@ class Order(models.Model):
     status = models.BooleanField(db_index=True, default=0)
     total = models.DecimalField(max_digits=6, decimal_places=2,)
     date = models.DateField(db_index=True)
+    
+    
     
     
 class OrderItem(models.Model):
--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,68 +1,62 @@
 from rest_framework import serializers
-from .models import Category, MenuItem, Cart, Order, OrderItem
 from django.contrib.auth.models import User
+from decimal import Decimal
 
-class CategorySerializer(serializers.ModelSerializer):
+from .models import Category, MenuItem, Cart, Order, OrderItem
+
+
+class CategorySerializer (serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'slug', 'title']
+        fields = ['id', 'title', 'slug']
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
-    category_id = serializers.IntegerField(write_only=True)
-
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+    # category = CategorySerializer(read_only=True)
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'featured', 'category', 'category_id']
+        fields = ['id', 'title', 'price', 'category', 'featured']
 
 
 class CartSerializer(serializers.ModelSerializer):
-    menuitem = MenuItemSerializer(read_only=True)
-    menuitem_id = serializers.IntegerField(write_only=True)
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+
+
+    def validate(self, attrs):
+        attrs['price'] = attrs['quantity'] * attrs['unit_price']
+        return attrs
 
     class Meta:
         model = Cart
-        fields = ['id', 'menuitem', 'menuitem_id', 'quantity', 'unit_price', 'price']
-        read_only_fields = ['unit_price', 'price']
-
-    def create(self, validated_data):
-        menuitem = MenuItem.objects.get(id=validated_data['menuitem_id'])
-        quantity = validated_data['quantity']
-        validated_data['unit_price'] = menuitem.price
-        validated_data['price'] = quantity * menuitem.price
-        validated_data['menuitem'] = menuitem
-        validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
+        fields = ['user', 'menuitem', 'unit_price', 'quantity', 'price']
+        extra_kwargs = {
+            'price': {'read_only': True}
+        }
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    menuitem = MenuItemSerializer(read_only=True)
-
     class Meta:
         model = OrderItem
-        fields = ['id', 'menuitem', 'quantity', 'unit_price', 'price']
+        fields = ['order', 'menuitem', 'quantity', 'price']
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    order_items = OrderItemSerializer(many=True, read_only=True, source='orderitem_set')
-    user = serializers.StringRelatedField(read_only=True)
-    delivery_crew = serializers.StringRelatedField()
-    delivery_crew_id = serializers.IntegerField(write_only=True, required=False)
+
+    orderitem = OrderItemSerializer(many=True, read_only=True, source='order')
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'delivery_crew', 'delivery_crew_id', 'status', 'total', 'date', 'order_items']
-        read_only_fields = ['total', 'date']
+        fields = ['id', 'user', 'delivery_crew',
+                  'status', 'date', 'total', 'orderitem']
 
-    def update(self, instance, validated_data):
-        if 'delivery_crew_id' in validated_data:
-            try:
-                crew = User.objects.get(id=validated_data['delivery_crew_id'])
-                instance.delivery_crew = crew
-            except User.DoesNotExist:
-                raise serializers.ValidationError("Invalid delivery crew ID")
-        if 'status' in validated_data:
-            instance.status = validated_data['status']
-        instance.save()
-        return instance
+
+class UserSerilializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','email']
